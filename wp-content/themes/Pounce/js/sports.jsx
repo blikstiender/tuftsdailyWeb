53ad91bf5739bd90a9d5a59d33d408fb1a3c3b3d
Meredith Clarke <maclarke15@gmail.com>
2017-03-03 13:53:09 -0500

    article view outline test

--- wp-content/themes/Pounce/js/sports.jsx
+++ wp-content/themes/Pounce/js/sports.jsx
@@ -3,9 +3,22 @@ import React, { Component } from 'react';
 export default class Sports extends Component {
     render() {
         return (
-            <h1>
-                Sports Page
-            </h1>
+            <div className="Header">
+                <h1> Article View </h1>
+                <div className="Title"> Title </div>
+                <div className="Author"> Author </Link>
+                <div className="Date"> Date </div>
+                <div className="Caption"> photo caption </div>
+
+            <div className="body"> TEXT </div>
+            
+            <hr className="bar-horizontal"/>
+            
+            <div className="Related"> Related articles </div>
+
+            <div className="Footer">
+
+            </div>
         );
     }
 }
