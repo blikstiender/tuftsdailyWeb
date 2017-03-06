96db0b4c60d27161075b1c06ace126c419006111
Meredith Clarke <maclarke15@gmail.com>
2017-03-05 19:42:52 -0500

    full article view - no styling, go to sports to view

--- wp-content/themes/Pounce/js/article.jsx
+++ wp-content/themes/Pounce/js/article.jsx
@@ -1,26 +1,73 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import Header from './header.jsx';
 
-export default class Article extends Component {
+var POST_URL = "https://tuftsdaily.com/wp-json/wp/v2/posts/163318"
+var AUTHOR_URL = "https://tuftsdaily.com/wp-json/wp/v2/users/"
+var MEDIA_URL = "https://tuftsdaily.com/wp-json/wp/v2/media/"
+
+export default class fullArticle extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {title: "", content: "", author: "", date_gmt:"", hasimage: false, image: ""};
+    }
+
+    componentDidMount() {
+        this.fetchData();
+    }
+
+    fetchData() {
+        fetch(POST_URL)
+            .then((response) => response.json())
+            .then((responseData) => {
+                this.setState({title: responseData.title.rendered, 
+                               content: responseData.content.rendered, 
+                               author_id: responseData.author
+                });
+                if(responseData.featured_media != 0){
+                    MEDIA_URL = MEDIA_URL + responseData.featured_media
+                    fetch(MEDIA_URL)
+                        .then((response) => response.json())
+                        .then((responseData) => {
+                            this.setState({hasimage: true, image: responseData.media_details.sizes.medium.source_url})
+                        })
+                }
+                AUTHOR_URL = AUTHOR_URL + this.state.author_id
+            fetch(AUTHOR_URL)
+                .then((response) => response.json())
+                .then((responseData) => {
+                    this.setState({author: responseData.name,
+                                    link: responseData.link})
+                })
+            })
+  }
+
     render() {
         return (
-            <div className="Header">
-                <h1> Article View </h1>
-                <div className="Title"> Title </div>
-                <div className="Author"> Author </Link>
-                <div className="Date"> Date </div>
-                <div className="Caption"> photo caption </div>
+            <div className="main-article">
+                <div className="article-header"> 
+                    <div className="row">
+                        <div className="col col-sm-8">
+                            <img className="pic-article" src={this.state.image} alt="article image" width="600" height="400"/>
+                        </div>
+                        <div className="col col-sm-4">
+                            <div className="lead-title-article"> {this.state.title} </div>
+                            <hr className="aricle-divider"/>
+                            <div className="author"> {this.state.author} </div>
+                            <div className="date-gmt"> {this.state.date_gmt} </div>
+                            <hr className="aricle-divider"/>
+                            <div className="media-caption"> {this.state.caption}</div>
+                        </div>
+                    </div>
+                </div>
 
-            <div className="body"> TEXT </div>
-            
-            <hr className="bar-horizontal"/>
-            
-            <div className="Related"> Related articles </div>
-
-            <div className="Footer">
-
+              <div className="container-fluid">
+                    <div className="main-page-article"> {this.state.content} </div>
+                    <hr className="aricle-divider"/>
+                    <div className="related-articles"> related articles </div>
+                    <div className="footer"> footer icons </div>
+                </div>
             </div>
-
         );
     }
 }
--- wp-content/themes/Pounce/js/sports.jsx
+++ wp-content/themes/Pounce/js/sports.jsx
@@ -3,26 +3,26 @@ import { Link } from 'react-router';
 import Header from './header.jsx';
 
 var POST_URL = "https://tuftsdaily.com/wp-json/wp/v2/posts/163318"
-var AUTHOR_URL = "https://tuftsdaily.com/wp-json/wp/v2/users/4447"
-var MEDIA_URL = "https://tuftsdaily.com/wp-json/wp/v2/media/163357"
+var AUTHOR_URL = "https://tuftsdaily.com/wp-json/wp/v2/users/"
+var MEDIA_URL = "https://tuftsdaily.com/wp-json/wp/v2/media/"
 
-export default class ArticleView extends Component {
-	constructor(props) {
-	    super(props);
-	    this.state = {title: "", content: "", author: "", date_gmt:"", hasimage: false, image: ""};
-	}
+export default class fullArticle extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {title: "", content: "", author: "", date_gmt:"", hasimage: false, image: ""};
+    }
 
-	componentDidMount() {
-	    this.fetchData();
-	}
+    componentDidMount() {
+        this.fetchData();
+    }
 
-	fetchData() {
- 		fetch(POST_URL)
- 			.then((response) => response.json())
- 			.then((responseData) => {
+    fetchData() {
+        fetch(POST_URL)
+            .then((response) => response.json())
+            .then((responseData) => {
                 this.setState({title: responseData.title.rendered, 
                                content: responseData.content.rendered, 
-                               author_n: responseData.author
+                               author_id: responseData.author
                 });
                 if(responseData.featured_media != 0){
                     MEDIA_URL = MEDIA_URL + responseData.featured_media
@@ -32,30 +32,41 @@ export default class ArticleView extends Component {
                             this.setState({hasimage: true, image: responseData.media_details.sizes.medium.source_url})
                         })
                 }
-                AUTHOR_URL = AUTHOR_URL + this.state.author_n
-	 		fetch(AUTHOR_URL)
-	 			.then((response) => response.json())
-	            .then((responseData) => {
-	                this.setState({author: "By " + responseData.name,
-	                				link: responseData.link})
-	            })
-	        })
+                AUTHOR_URL = AUTHOR_URL + this.state.author_id
+            fetch(AUTHOR_URL)
+                .then((response) => response.json())
+                .then((responseData) => {
+                    this.setState({author: responseData.name,
+                                    link: responseData.link})
+                })
+            })
   }
 
     render() {
         return (
-        	<div className="main-article">
-        	    <img className="pic-article" src={this.state.image} alt="article image"/>
-            	<div className="lead-title-article"> {this.state.title} </div>
-            	<div className="container-fluid">
-	            	<div className="author"> {this.state.author} </div>
-    	        	<div className="date-gmt"> {this.state.date_gmt} </div>
-        	   		<div className="media-caption"> {this.state.caption}</div>
-            		<div className="article-body"> {this.state.content} </div>
-            		<hr className="aricle-divider"/>
-           			<div className="related-articles"> related articles </div>
-            		<div className="footer"> footer icons </div>
-            	</div>
+            <div className="main-article">
+                <div className="article-header"> 
+                    <div className="row">
+                        <div className="col col-sm-8">
+                            <img className="pic-article" src={this.state.image} alt="article image" width="600" height="400"/>
+                        </div>
+                        <div className="col col-sm-4">
+                            <div className="lead-title-article"> {this.state.title} </div>
+                            <hr className="aricle-divider"/>
+                            <div className="author"> {this.state.author} </div>
+                            <div className="date-gmt"> {this.state.date_gmt} </div>
+                            <hr className="aricle-divider"/>
+                            <div className="media-caption"> {this.state.caption}</div>
+                        </div>
+                    </div>
+                </div>
+
+              <div className="container-fluid">
+                    <div className="main-page-article"> {this.state.content} </div>
+                    <hr className="aricle-divider"/>
+                    <div className="related-articles"> related articles </div>
+                    <div className="footer"> footer icons </div>
+                </div>
             </div>
         );
     }
