621c2b9219c64f9c57b982edacf201530b5aa956
Joe Campbell <jcamp1095@gmail.com>
2017-03-12 22:39:04 -0400

    article view works as component now

--- wp-content/themes/Pounce/js/sports.jsx
+++ wp-content/themes/Pounce/js/sports.jsx
@@ -1,73 +1,18 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import Header from './header.jsx';
+import Article from './article.jsx';
 
 var POST_URL = "https://tuftsdaily.com/wp-json/wp/v2/posts/163318"
 var AUTHOR_URL = "https://tuftsdaily.com/wp-json/wp/v2/users/"
 var MEDIA_URL = "https://tuftsdaily.com/wp-json/wp/v2/media/"
 
-export default class fullArticle extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {title: "", content: "", author: "", date_gmt:"", hasimage: false, image: ""};
-    }
-
-    componentDidMount() {
-        this.fetchData();
-    }
-
-    fetchData() {
-        fetch(POST_URL)
-            .then((response) => response.json())
-            .then((responseData) => {
-                this.setState({title: responseData.title.rendered, 
-                               content: responseData.content.rendered, 
-                               author_id: responseData.author
-                });
-                if(responseData.featured_media != 0){
-                    MEDIA_URL = MEDIA_URL + responseData.featured_media
-                    fetch(MEDIA_URL)
-                        .then((response) => response.json())
-                        .then((responseData) => {
-                            this.setState({hasimage: true, image: responseData.media_details.sizes.medium.source_url})
-                        })
-                }
-                AUTHOR_URL = AUTHOR_URL + this.state.author_id
-            fetch(AUTHOR_URL)
-                .then((response) => response.json())
-                .then((responseData) => {
-                    this.setState({author: responseData.name,
-                                    link: responseData.link})
-                })
-            })
-  }
+export default class Sports extends Component {
+    
 
     render() {
         return (
-            <div className="main-article">
-                <div className="article-header"> 
-                    <div className="row">
-                        <div className="col col-sm-8">
-                            <img className="pic-article" src={this.state.image} alt="article image" width="600" height="400"/>
-                        </div>
-                        <div className="col col-sm-4">
-                            <div className="lead-title-article"> {this.state.title} </div>
-                            <hr className="aricle-divider"/>
-                            <div className="author"> {this.state.author} </div>
-                            <div className="date-gmt"> {this.state.date_gmt} </div>
-                            <hr className="aricle-divider"/>
-                            <div className="media-caption"> {this.state.caption}</div>
-                        </div>
-                    </div>
-                </div>
-
-              <div className="container-fluid">
-                    <div className="main-page-article"> {this.state.content} </div>
-                    <hr className="aricle-divider"/>
-                    <div className="related-articles"> related articles </div>
-                    <div className="footer"> footer icons </div>
-                </div>
-            </div>
+            <Article />
         );
     }
 }
